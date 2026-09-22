86daf7f063dbe0baee7a34f1fc23cb0038809a55
Anthony Castaneda <lcastaa.aql@gmail.com>
2026-09-22 12:10:18 -0500

    Bump package version to 0.3.0 for the logistic shadow model

--- src/kalshi_bot/__init__.py
+++ src/kalshi_bot/__init__.py
@@ -7,4 +7,4 @@ try:
     __version__ = metadata.version("kalshi-bot")
 except metadata.PackageNotFoundError:
     # Not installed (e.g. running straight from source) - keep in sync with pyproject.toml.
-    __version__ = "0.2.0"
+    __version__ = "0.3.0"
